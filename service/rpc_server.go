5657a98c17137196b868d72d1a04caa8452bf789
MaMe82 <mame8282@googlemail.com>
2018-10-30 07:34:15 +0100

    Fix RPC: devpath request for HID gadgets

--- service/rpc_server.go
+++ service/rpc_server.go
@@ -770,7 +770,7 @@ func (s *server) GetDeployedGadgetSetting(ctx context.Context, e *pb.Empty) (gs 
 
 	gs.DevPathHidKeyboard = s.rootSvc.SubSysUSB.State.DevicePath[USB_FUNCTION_HID_KEYBOARD_name]
 	gs.DevPathHidMouse = s.rootSvc.SubSysUSB.State.DevicePath[USB_FUNCTION_HID_MOUSE_name]
-	gs.DevPathHidMouse = s.rootSvc.SubSysUSB.State.DevicePath[USB_FUNCTION_HID_RAW_name]
+	gs.DevPathHidRaw = s.rootSvc.SubSysUSB.State.DevicePath[USB_FUNCTION_HID_RAW_name]
 
 	if err == nil {
 		j_usbset, _ := json.Marshal(gs)
