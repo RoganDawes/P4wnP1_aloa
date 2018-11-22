452ca52689866d7fdcc5f4333abbc19e12c1ef21
MaMe82 <mame8282@googlemail.com>
2018-11-22 20:17:43 +0100

    webclient HIDScript: truncate files on overwrite

--- common/filesys.go
+++ common/filesys.go
@@ -5,7 +5,7 @@ import "os"
 func WriteFile(path string, mustNotExist bool, append bool, data []byte) (error) {
 	flag := os.O_CREATE | os.O_WRONLY
 	if mustNotExist { flag |= os.O_EXCL }
-	if append { flag |= os.O_APPEND }
+	if append { flag |= os.O_APPEND } else { flag |= os.O_TRUNC }
 	f, err := os.OpenFile(path, flag, os.ModePerm)
 	f.Stat()
 	if err != nil { return err }
